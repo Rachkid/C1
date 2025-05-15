6de3c3dda541667c40efac181e43d78459cb8241
Rachkid <abouzeidirachid97@gmail.com>
2025-05-15 21:27:27 +0100

    Update and rename count_files.sh to find_string.sh

--- count_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# ce code affiche la liste des fichiers dans un répertoire 
-read dossier
-count=$(ls "$dossier" | wc -l)
-counts=$(echo "$count")
-echo " le dossier $dossier contient $counts fichier(s)."
